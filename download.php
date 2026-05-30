3675f657a5111df0c78ea872142b1cde883c6f28
animattos <estudioanimattos@gmail.com>
2026-05-29 21:56:07 -0300

    Delete download.php

--- download.php
+++
@@ -1,3 +0,0 @@
-<?php
-$arquivo= $_GET['baixar'];header("Content-type: application/save"); header("Content-Length:".filesize($arquivo)); header('Content-Disposition: attachment; filename="' . $arquivo. '"');readfile("$arquivo");
-?>
